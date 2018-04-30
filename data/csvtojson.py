164dd29f31a29e0ef724c95bd7b01b10cfaf1c99
Akhilesh Jichkar <35941109+AkhiJix@users.noreply.github.com>
2018-04-30 14:57:29 -0700

    Update csvtojson.py

--- data/csvtojson.py
+++ data/csvtojson.py
@@ -3,7 +3,7 @@ csv_file = open('songdata.csv','r')
 json_file = open('LyrJson.json','w')
 json_file.write("[\n")
 for line in csv.reader(csv_file):
-    json_file.write(",\n\t{\n")
+    json_file.write(",\n\t{\n")         #Remove comma from the second line of the generated JSON file.
     json_file.write( "\t\t\"artist\": \""+line[0].replace(",","").replace("\'","").replace("\"","")+"\",\n")
     json_file.write( "\t\t\"songname\": \""+line[1].replace(",","").replace("\'","").replace("\"","")+"\",\n")
     json_file.write( "\t\t\"lyrics\": \""+line[2].replace("\n","").replace(",","").replace("\'","").replace("\"","")+"\"\n")
